a138a648b9673fdc3fbe39009fd42c9744e3785f
shuo xu <shuo.xu@2021.icam.fr>
2024-11-27 19:47:32 +0100

    backend: database queries

--- routes/ideas.js
+++ routes/ideas.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router(); 
+const Idea = require('../models/Idea');
 
 const ideas = [
     {
@@ -25,78 +26,91 @@ const ideas = [
     },
 ];
   
+
+
+
 // Create a route to get all ideas
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const ideas = await Idea.find();
     res.json({ success: true, data: ideas });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong' })
+  } 
 });
+
+
+
+
 
 // Create a route to get a single idea
 // specified ':id' in the route, so we can access that route parameter using 'req.params.id'
-router.get('/:id', (req, res) => {
-    // find() loop through each idea and gives the idea with the id that matches the route parameter
-    // '+' turn a string into a number, '+req.params.id' gives the number of the id
-    const idea = ideas.find((idea) => idea.id === +req.params.id);
-    
-    //if idea didnt found, return 404 status code and an error
-    if (!idea) {
-        return res
-            .status(404)
-            .json({ success: false, error: 'Resource not found' });
-    }
-
-    // if idea found, send back the idea as a successful JSON response
-    res.json({ sucess: true, data: idea });
+router.get('/:id', async (req, res) => {
+  try {
+    const idea = await Idea.findById(req.params.id); 
+    res.json({ success: true, data: idea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({success: false, error: 'Something went wrong'})
+  }
 });
 
+
+
+
 // Add an idea
-router.post('/', (req, res) => {
-    const idea = {
-        id: ideas.length + 1,
-        text: req.body.text,
-        tag: req.body.tag,
-        username: req.body.username,
-        date: new Date().toISOString().slice(0, 10),
-    }
+router.post('/', async (req, res) => {
+  //create an instance of Idea 
+  const idea = new Idea({
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+  });
 
-    ideas.push(idea);
-
-    // the middleware allow us to access the data that we send with the request
-    res.json({ success: true, data: idea });
+  try {
+    const savedIdea = await idea.save();
+    res.json({ sucess: true, data: savedIdea }); 
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong' });
+  }
 });
 
 
 
 //update idea
-router.put('/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-  if (!idea) {
-    return res
-      .status(404)
-      .json({ success: false, error: 'Resource not found' });
-  } else {
-    idea.text = req.body.text || idea.text;
-    idea.tag = req.body.tag || idea.tag;
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedIdea = await Idea.findByIdAndUpdate(
+      // the id  
+      req.params.id,
+      { // updated text and tag by using $set
+        $set: {
+        text: req.body.text,
+        tag: req.body.tag
+         }
+      },
+      // if the id (req.params.id) didnt exist, a new id will be created
+      { new: true}, 
+    );
+    res.json({ success: true, data: updatedIdea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong' })
   }
-
-  res.json({ success: true, data: idea });
-})
+});
 
 
 
 // delete idea
-router.delete('/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === + req.params.id);
-
-  if (!idea) {
-    return res
-      .status(404)
-      .json({ success: false, error: 'Resource not found' });
-  } else {
-    const index = ideas.indexOf(idea); 
-    ideas.splice(index, 1);
-
+router.delete('/:id', async (req, res) => {
+  try {
+    await Idea.findByIdAndDelete(req.params.id);
     res.json({ success: true, data: {} });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, error: 'Something went wrong' });
   }
 });
 
