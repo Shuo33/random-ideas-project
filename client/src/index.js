b30d037b08fa0218f0344d36203f62bbb8df9163
shuo xu <shuo.xu@2021.icam.fr>
2024-11-28 11:44:43 +0100

    frontend: client folder setup

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+// CommonJS modules with webpack.config.js 
+import './css/style.css';
+
+// import fontawesome
+import '@fortawesome/fontawesome-free/css/all.css';
+
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 // dotenv is a library that allow us to store environment variables in a file called .env, we'll use this to store our MongoDB connection string, which is 'MONGO_URI'
@@ -10,6 +11,9 @@ const connectDB = require('./config/db');
 connectDB();
 
 const app = express();
+
+// make the public folder static
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware is those methods/functions/operations that are called BETWEEN processing the Request and sending the Response in your application method
 // Body parser middleware: parsing data in the body
@@ -30,6 +34,5 @@ const ideasRouter = require('./routes/ideas');
 
 // app.use() helps to add middleware to the app. Here we adding the ideasRouter middleware to the '/api/ideas' route, which means all the routes in the ideasRouter will be prefixed with '/api/ideas' automatically. 
 app.use('/api/ideas', ideasRouter);
-
 
 app.listen(PORT, ()=> console.log(`Server listening on port ${PORT}`));
