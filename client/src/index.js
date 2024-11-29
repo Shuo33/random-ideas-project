d6f95c17b1066bfed5df5eabcb06b42119f48ce7
shuo xu <shuo.xu@2021.icam.fr>
2024-11-29 10:59:26 +0100

    frontend: ideaList component

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,22 @@ import './css/style.css';
 // import CSS fontawesome
 import '@fortawesome/fontawesome-free/css/all.css';
 
+
 // import Modal
 import Modal from './components/Modal';
 // instantiate the Modal
 const modal = new Modal();
+
 
 //import IdeaForm
 import IdeaForm from './components/IdeaForm';
 //instantiate the IdeaForm
 const ideaForm = new IdeaForm();
 ideaForm.render();
+
+
+//import IdeaList
+import IdeaList from './components/IdeaList';
+//instantiate the IdeaList
+const ideaList = new IdeaList();
+ideaList.render();
